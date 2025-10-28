c83051e329eb9e6780d5a3d871b7f7bfc2624cfc
Jaider Vargas <jaider.vargas-n@mail.escuelaing.edu.co>
2025-10-27 23:04:30 -0500

    health controller

--- src/main/java/com/prestek/coltefinanciera/controller/HealthController.java
+++ src/main/java/com/prestek/coltefinanciera/controller/HealthController.java
@@ -1,11 +1,17 @@
 package com.prestek.coltefinanciera.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RequestMapping("/api/health")
+@CrossOrigin(origins = "*")
+@Slf4j
+@RestController
 public class HealthController {
     @GetMapping
     public Map<String, Object> health() {
