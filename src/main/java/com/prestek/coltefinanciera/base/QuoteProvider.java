37235915e2d2b73ced3320f38fd9303547692591
Jaider Vargas <jaider.vargas-n@mail.escuelaing.edu.co>
2025-10-28 10:03:00 -0500

    use core library

--- src/main/java/com/prestek/coltefinanciera/base/QuoteProvider.java
+++
@@ -1,9 +0,0 @@
-package com.prestek.coltefinanciera.base;
-
-import com.prestek.coltefinanciera.dto.QuoteDto;
-import com.prestek.coltefinanciera.request.QuoteRequest;
-
-public interface QuoteProvider {
-    String code();                    // Identificador del banco
-    QuoteDto quote(QuoteRequest req);// Calcula la cotización
-}
